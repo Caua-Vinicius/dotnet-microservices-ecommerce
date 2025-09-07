424e1a7f51adcbdca032f98ce4aed2c53c48301c
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2025-09-06 22:33:58 -0300

    feat: add User entity class to AuthService domain

---
+++ AuthService/Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthService.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
