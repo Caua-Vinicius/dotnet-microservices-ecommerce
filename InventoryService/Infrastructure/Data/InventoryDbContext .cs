9d047a2a8f995ae164ae05f22fc8667ed0a7a4bb
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2025-09-13 19:48:18 -0300

    feat: add InventoryDbContext for managing Products and StockHistories

---
+++ InventoryService/Infrastructure/Data/InventoryDbContext .cs
@@ -0,0 +1,12 @@
+using InventoryService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryService.Infrastructure.Data
+{
+    public class InventoryDbContext : DbContext
+    {
+        public InventoryDbContext(DbContextOptions<InventoryDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<StockHistory> StockHistories { get; set; }
+    }
+}
