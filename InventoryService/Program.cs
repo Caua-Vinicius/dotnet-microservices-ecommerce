e7cd1798d7e93ba619c30abea82330dd7c351ca9
Cauã Vinicius Gomes Monteiro <cauavinicius.contato@gmail.com>
2025-09-13 19:44:24 -0300

    feat: update InventoryService project configuration and add necessary package references

--- InventoryService/Program.cs
+++ InventoryService/Program.cs
@@ -1,7 +1,21 @@
+using InventoryService.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+// Add MediatR
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+// DbContext 
+builder.Services.AddDbContext<InventoryDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -13,4 +27,5 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapControllers();
 app.Run();
